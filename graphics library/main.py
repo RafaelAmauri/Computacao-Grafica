fec7024fa171ddc4b1118e34a897a516192de9d5
Rafael <amauri.rafael33@gmail.com>
2023-08-03 16:51:12 -0300

    Add rotation operation

--- graphics library/main.py
+++ graphics library/main.py
@@ -1,4 +1,5 @@
-import scale, translation, utils
+import scale, translation, rotation, utils
+import math
 
 # Triangulo 3-4-5
 triangulo1 = {
@@ -11,9 +12,10 @@ quadrado1 = {
                 "y": [1, 15, 15, 1, 1]
 }
 
-utils.show_figure(quadrado1)
+utils.show_figure(triangulo1)
 
 #triangulo1_transformed = translation.translation2d(triangulo1.copy(), axis="both", y_padding=2, x_padding=2)
-quadrado1_scaled = scale.scale2d(quadrado1.copy(), axis="both", x_scale=1, y_scale=0.5)
+#quadrado1_scaled = scale.scale2d(quadrado1.copy(), axis="both", x_scale=1, y_scale=0.5)
+q1_rotated = rotation.rotation2d(quadrado1, direction="clockwise", angle=90)
 
-utils.show_figure(quadrado1, quadrado1_scaled)
+utils.show_figure(quadrado1, q1_rotated)
