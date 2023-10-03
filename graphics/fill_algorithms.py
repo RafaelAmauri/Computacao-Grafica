9021c1fb177bbe146e7697ea38a424661c81d5a6
Rafael <amauri.rafael33@gmail.com>
2023-10-03 16:28:51 -0300

    test beta'

--- graphics/fill_algorithms.py
+++ graphics/fill_algorithms.py
@@ -4,5 +4,6 @@ from typing import Tuple, List
 def boundaryFill(graph, startPoint: Tuple[int, int], fillColor: str, boundaryColors: List):
     x, y = startPoint
     graph.DrawPoint((x, y), 1, color=str)
-
+    
+    # NovO COMMENT
     # if color x + 1, y not in boundaryColors
